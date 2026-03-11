ff66646511d28a59c560b982dba0cbe5e65a3538
watanabe-toshi <109321773+watanabe-toshi@users.noreply.github.com>
2026-03-11 22:34:54 +0900

    Add AWS region and project name variables

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-northeast-1"
+}
+
+variable "project_name" {
+  description = "Project name prefix"
+  type        = string
+  default     = "iac-ai-demo"
+}
